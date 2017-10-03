60ce91bfcb6c7eaf995f433bbea15e8fe3ae07cd
Kitty Jacobs <kitty.jacobs@immalle.eu>
2017-10-03 09:10:27 +0200

    Added elements

--- CookieClicker/MainWindow.xaml.cs
+++ CookieClicker/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace CookieClicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int c = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if(c < 50)
+            {
+                c = c + 1;
+            } else if(c < 100)
+            {
+                c = c + 2;
+                label2.Content = "x2";
+            } else if(c < 500) {
+                c = c + 3;
+                label2.Content = "x3";
+                label3.Content = "Keep going!";
+            } else if(c < 1000)
+            {
+                c = c + 4;
+                label2.Content = "x4";
+            } else if(c < 2000) {
+                c = c + 5;
+                label2.Content = "x5";
+            } else if(c < 4000)
+            {
+                c = c + 6;
+                label2.Content = "x6";
+            }
+
+            label1.Content = c;
         }
     }
 }
